e6400d6dbe472318b9cb0569ee69054167b4acf5
alirabah93 <abu.rabah.93@gmail.com>
2022-10-12 00:50:46 +0300

    add and edit cars backend is ready, wating for front end

--- palrent/car_app/urls.py
+++ palrent/car_app/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     path("payment_confirmation/", views.payment_confirmation),
     path("confirm_book/", views.confirm_book),
     path("provider_dashboard/", views.provider_dashboard),
-    path("add_edit/", views.add_edit),
+    path("add_car/", views.add_car),
     path("insert_car/", views.insert_car),
+    path("edit_car/<car_id>/", views.edit_car),
+    path("edit_my_car/<car_id>/", views.edit_my_car),
+
 ]
--- palrent/car_app/views.py
+++ palrent/car_app/views.py
@@ -54,19 +54,51 @@ def provider_dashboard(request):
     return render(request,"provider_dashboard.html")
 
 
-def add_edit(request):
-    return render(request,"add_edit.html")
+def add_car(request):
+    print("this works here")
+
+    context = {
+        'cars': Car.objects.filter(provider=request.session["provider_id"])
+    }
+    return render(request,"add_car.html", context)
 
 def insert_car(request):
 
     Car.objects.create(
             brand = request.POST['brand'],
             model = request.POST['model'],
+            color = request.POST['color'],
             production_year = request.POST['production_year'],
             plate_number = request.POST['plate_number'],
             price = request.POST['price'],
             provider = Provider.objects.get(id=request.session['provider_id'])
         )
-    return redirect("/add_edit")
+    return redirect("/my_dashboard/add_car")
+
+def edit_car(request, car_id):
+
+    context = {
+        'garage': Car.objects.filter(provider=request.session["provider_id"]),
+        'car_id': car_id,
+        'car': Car.objects.get(id=car_id)
+    }
+    return render(request,"edit_car.html", context)
+
+def edit_my_car(request, car_id):
+
+    print("this works here")
+
+    # car_id = request.POST['car_id']
+    c = Car.objects.get(id=car_id)
+    c.brand = request.POST['brand']
+    c.model = request.POST['model']
+    c.color = request.POST['color']
+    c.production_year = request.POST['production_year']
+    c.plate_number = request.POST['plate_number']
+    c.price = request.POST['price']
+    c.save()
+
+    return redirect("/my_dashboard/edit_car/"+car_id)
+
 
 
